3d54404aeef041d371fb34626f583a6ca5214500
Yuriferr <yurifernandespreto@gmail.com>
2023-06-05 19:42:47 -0300

    ajuste mobile

--- web/src/components/Projects/index.jsx
+++ web/src/components/Projects/index.jsx
@@ -18,7 +18,7 @@ export default function Projects() {
       </div>
 
       <p>
-        Meus Recentes Projetos
+        Meus Projetos Recentes
       </p>
 
       <div className="Cards">
--- web/src/components/Skills/index.jsx
+++ web/src/components/Skills/index.jsx
@@ -23,14 +23,11 @@ export default function Skills(){
                 <Cards icon={<SiCss3 size={60}/>}/>
                 <Cards icon={<SiJavascript size={60}/>}/>
                 <Cards icon={<SiTypescript size={60}/>}/>
-
                 <Cards icon={<SiNodedotjs size={60}/>}/>
                 <Cards icon={<SiPython size={60}/>}/>
                 <Cards icon={<FaJava size={60}/>}/>
-
                 <Cards icon={<SiMysql size={60}/>}/>
                 <Cards icon={<SiSqlite size={60}/>}/>
-
                 <Cards icon={<SiGithub size={60}/>}/>
                 <Cards icon={<SiGitlab size={60}/>}/>
                 <Cards icon={<SiFigma size={60}/>}/>
